a3f2889d424b2572b6da6143d230555a3f63a0e2
mugunthan17 <mugunthannagalingam17@gmail.com>
2025-07-17 15:49:35 +0530

    Initial commit - added SurvAI-frontEnd folder

--- src/assets/Data/customQuestionData.js
+++
@@ -1,9 +0,0 @@
-// src/data/customQuestionData.js
-
-export const answer = `Most users appreciated the comfort, fit, and overall feel of the clothing. Several comments specially highlighted the softness of the fabric, the accurate sizing, and how well the items matched expectations.
-
-Frequent Mentions:
-- Very comfortable to wear
-- Fits Perfectly
-- Soft and breathable fabric
-- True to size and fitting`;
