2d2f9e11478b59e8f22fa159cdec49ebf4e6f33d
mugunthan17 <mugunthannagalingam17@gmail.com>
2025-07-19 10:33:41 +0530

    v.1.0

--- SurvAI-backEnd/server.js
+++ SurvAI-backEnd/server.js
@@ -6,10 +6,15 @@ import analyzeRoutes from './routes/analyze.js'
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+const APP_NAME = process.env.APP_NAME;
 app.use(cors());
 app.use(express.json());
 app.use('/api/analyze', analyzeRoutes);
+
+app.get('/', (req, res) => {
+    res.send(`🎉 ${APP_NAME} Server is running successfully!`);
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server running on http://localhost:${PORT}`)
--- SurvAI-frontEnd/src/components/FeaturePageComponents/FormatBytes.jsx
+++
@@ -1,6 +0,0 @@
-export const formatBytes = (bytes) => {
-  const sizes = ["Bytes", "KB", "MB", "GB"];
-  if (bytes === 0) return "0 Byte";
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-  return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
-};
