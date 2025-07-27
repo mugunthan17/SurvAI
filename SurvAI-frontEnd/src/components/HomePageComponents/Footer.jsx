79bc6c062be713becc2819d4e986f37ec65f0432
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-07-27 11:38:55 +0530

    v.1.1 Refactord  the Year to dynamic and updated the mailto support mail

--- SurvAI-frontEnd/src/components/HomePageComponents/Footer.jsx
+++ SurvAI-frontEnd/src/components/HomePageComponents/Footer.jsx
@@ -1,5 +1,7 @@
 import Logo from './../../assets/Logo.png';
 
+const year = new Date().getFullYear();
+
 const Footer = () => {
   return (
     <footer className="text-center pt-8 text-gray-700 border-t-2 border-[#2A3BFF]">
@@ -9,7 +11,7 @@ const Footer = () => {
       </p>
       <div className="flex flex-col lg:flex-row md:flex-row text-lg justify-center items-center gap-4 lg:gap-8 md:gap-8 mb-6">
         <a
-          href="#"
+          href="mailto:survaicontact@gmail.com"
           className="pop text-[#0C0C0C] flex items-center gap-2 font-medium hover:underline"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z"/></svg>
@@ -18,7 +20,7 @@ const Footer = () => {
       </div>
 
       <p className="alexa text-base text-[#555555] mt-10 mb-4">
-        © 2025 SurvAI. All Rights Reserved
+        © {year} SurvAI. All Rights Reserved
       </p>
     </footer>
   );
